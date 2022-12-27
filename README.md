e9ad34857b16727b8483f04e4fac4612d61e6769
Armaan Badhan <armaanbadhan07@gmail.com>
2022-12-28 01:16:17 +0530

    arrow moves now

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
 trying to make some retro games in terminal only
 
+
+# to play:
+
 `pip install -r requirements.txt`
+
 `python menu.py`
 
-# contributing:
 
-some resources:
+
+some resources on ansi:
+
 http://www.lihaoyi.com/post/BuildyourownCommandLinewithANSIescapecodes.html
+
 https://en.wikipedia.org/wiki/ANSI_escape_code
+
 https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
 
+https://tldp.org/HOWTO/Bash-Prompt-HOWTO/x361.html
--- menu.py
+++ menu.py
@@ -1,13 +1,19 @@
 from os import get_terminal_size
 import os
+import msvcrt
 
 # TODO: hide cursor
 # ref: https://superuser.com/questions/1496322/how-do-i-remove-hide-the-cursor-the-blinking-underscore-character-in-cmd-exe
 
 # ANSI Characters
 RESET = "\033c"
-UP = '\033[1A'
-DOWN = '\033[1B'
+HOME = "\033[;H"
+
+UP =      '\033[1A'
+DOWN =    '\033[1B'
+FORWARD = '\033[1C'
+BACK =    '\033[1D'
+
 CLEAR = '\033[2K'
 GO_TO_START_OF_LINE = "\r"
 # make sure to use \r after if using at start of a line
@@ -27,7 +33,7 @@ term_size = get_terminal_size()
 MAX_WIDTH = term_size.columns
 MAX_HEIGHT = term_size.lines
 
-banner = [
+BANNER = [
     "███████╗██╗   ██╗███╗   ██╗     ███████╗ ██████╗  ██████╗██╗███████╗████████╗██╗   ██╗",
     "██╔════╝██║   ██║████╗  ██║     ██╔════╝██╔═══██╗██╔════╝██║██╔════╝╚══██╔══╝╚██╗ ██╔╝",
     "█████╗  ██║   ██║██╔██╗ ██║     ███████╗██║   ██║██║     ██║█████╗     ██║    ╚████╔╝ ",
@@ -35,7 +41,7 @@ banner = [
     "██║     ╚██████╔╝██║ ╚████║     ███████║╚██████╔╝╚██████╗██║███████╗   ██║      ██║   ",
     "╚═╝      ╚═════╝ ╚═╝  ╚═══╝     ╚══════╝ ╚═════╝  ╚═════╝╚═╝╚══════╝   ╚═╝      ╚═╝   ",
 ]
-bs = """
+INFO = """
 Ned Bosham reopened the arcade under the name "Fun Society Amusement, LLC". In August 2013, Harold Bosham 
 pressed his father to sell the property; when that failed, Harold stole his twin brother's hunting rifle
 and shot Ned in the face. The rifle kicked back so hard, he tripped and fell out of a 20-story window. 
@@ -44,25 +50,82 @@ cellmate. When Romero was released, he received the keys from Clyde, who urged h
 Romero took advantage of the building's legal limbo to create an anonymous safehouse.
 """
 
+
+ARROW_POSITION = {   # (call param) : (down, right)
+    (0, 0): (20, 12),
+    (0, 1): (20, 38),
+    (1, 0): (21, 12),
+    (1, 1): (21, 38),
+    (2, 0): (22, 12),
+    (2, 1): (22, 38),
+    (3, 0): (24, 38),
+    (3, 1): (24, 38),
+}
+
+
+class Arrow:
+    def __init__(self) -> None:
+        self.arrow  = "    -->"
+        self.eraser = "       "
+        self.position = (0, 0)
+        self.display(self.position)
+
+    def set_initial(self):
+        return
+    
+    def display(self, pos):
+        self.erase()
+        self.position = pos
+        posi = ARROW_POSITION[pos]
+        print(HOME + DOWN*posi[0] + FORWARD*posi[1] + RESET_COLOR + self.arrow, end='\r')
+
+    
+    def erase(self):
+        posi = ARROW_POSITION[self.position]
+        print(HOME + DOWN*posi[0] + FORWARD*posi[1] + RESET_COLOR + self.eraser, end='\r')
+        return
+
+
+
+
 print(RESET)
 
-for ban in banner:
+for ban in BANNER:
     print(RED + "        " + ban)
 
-print(bs)
+print(INFO)
 
-print("***Welcome to Fun-Society***")       # TODO: make it blink somehow
+print("***Welcome to Fun-Society***\n\n")       # TODO: make it blink somehow
 
-print(DOWN)
-
-print(LIGHT_CYAN + GO_TO_START_OF_LINE + "choose the game you wanna play:" + DOWN + LIGHT_BLUE)
+print(LIGHT_CYAN + GO_TO_START_OF_LINE + "choose the game you wanna play(w/a/s/d/enter):\n" + LIGHT_BLUE)
 
 print(" "*20 + "game 1" + " "*20 + "game 2")
 print(" "*20 + "game 3" + " "*20 + "game 4")
 print(" "*20 + "game 5" + " "*20 + "game 6")
 print("")
 print(" "*20 + "      " + " "*20 + UNDERLINE + "exit")
-print(RESET_COLOR)
 
-# TODO: make a moving arrow which doesnt refresh everything
+arrow = Arrow()
+
+
 # TODO: border around the whole thing?
+
+
+point_to_x, point_to_y = 0, 0
+
+while True:
+    arrow.display((point_to_x, point_to_y))
+    char = msvcrt.getch().decode("utf-8")
+
+    if char in ('a', 'd'):
+        point_to_y = (point_to_y + 1) % 2
+    elif char == 'w':
+        point_to_x = (point_to_x + 3) % 4
+    elif char == 's':
+        point_to_x = (point_to_x + 1) % 4
+    elif char == '\r':
+        print("you chose", point_to_x, point_to_y)
+        break
+
+
+print(RESET_COLOR)
