303cd0a527b47feab5483f952b75cdd28acea862
Armaan Badhan <armaanbadhan07@gmail.com>
2022-12-27 21:26:16 +0530

    why unicode when you can ansi

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 trying to make some retro games in terminal only
+
+`pip install -r requirements.txt`
+`python menu.py`
+
+# contributing:
+
+some resources:
+http://www.lihaoyi.com/post/BuildyourownCommandLinewithANSIescapecodes.html
+https://en.wikipedia.org/wiki/ANSI_escape_code
+https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
+
--- menu.py
+++ menu.py
@@ -1,5 +1,22 @@
-from colorama import Fore, init
 from os import get_terminal_size
+import os
+
+# https://stackoverflow.com/questions/37774983/clearing-the-screen-by-printing-a-character
+
+
+# ANSI Characters
+RESET = "\033c"
+UP = '\033[1A'
+DOWN = '\033[1B'
+CLEAR = '\033[2K'
+RESET_COLOR = '\033[0m'
+GO_TO_START_OF_LINE = "\r"
+# make sure to use \r after if using at start of a line
+RED = '\033[0;31m'
+LIGHT_BLUE = '\033[1;34m'
+
+
+os.system('mode con: cols=100 lines=40')
 
 
 term_size = get_terminal_size()
@@ -7,21 +24,21 @@ term_size = get_terminal_size()
 MAX_WIDTH = term_size.columns
 MAX_HEIGHT = term_size.lines
 
-init(autoreset=True)
+banner = [
+    "███████╗██╗   ██╗███╗   ██╗     ███████╗ ██████╗  ██████╗██╗███████╗████████╗██╗   ██╗",
+    "██╔════╝██║   ██║████╗  ██║     ██╔════╝██╔═══██╗██╔════╝██║██╔════╝╚══██╔══╝╚██╗ ██╔╝",
+    "█████╗  ██║   ██║██╔██╗ ██║     ███████╗██║   ██║██║     ██║█████╗     ██║    ╚████╔╝ ",
+    "██╔══╝  ██║   ██║██║╚██╗██║     ╚════██║██║   ██║██║     ██║██╔══╝     ██║     ╚██╔╝  ",
+    "██║     ╚██████╔╝██║ ╚████║     ███████║╚██████╔╝╚██████╗██║███████╗   ██║      ██║   ",
+    "╚═╝      ╚═════╝ ╚═╝  ╚═══╝     ╚══════╝ ╚═════╝  ╚═════╝╚═╝╚══════╝   ╚═╝      ╚═╝   ",
+]
 
-banner = """
-███████╗██╗   ██╗███╗   ██╗     ███████╗ ██████╗  ██████╗██╗███████╗████████╗██╗   ██╗
-██╔════╝██║   ██║████╗  ██║     ██╔════╝██╔═══██╗██╔════╝██║██╔════╝╚══██╔══╝╚██╗ ██╔╝
-█████╗  ██║   ██║██╔██╗ ██║     ███████╗██║   ██║██║     ██║█████╗     ██║    ╚████╔╝ 
-██╔══╝  ██║   ██║██║╚██╗██║     ╚════██║██║   ██║██║     ██║██╔══╝     ██║     ╚██╔╝  
-██║     ╚██████╔╝██║ ╚████║     ███████║╚██████╔╝╚██████╗██║███████╗   ██║      ██║   
-╚═╝      ╚═════╝ ╚═╝  ╚═══╝     ╚══════╝ ╚═════╝  ╚═════╝╚═╝╚══════╝   ╚═╝      ╚═╝   
-"""   # 86
 
-print(Fore.RED +  banner)
+print(RESET)
 
-print(Fore.GREEN + "choose the game you wanna play:")
-print(Fore.GREEN + "                    snake")
-print(Fore.GREEN + "                    pong")
-print(Fore.GREEN + "                    tetris")
-print(Fore.GREEN + "                    quit")
+for ban in banner:
+    print(RED + "        " + ban)
+
+print(DOWN*5)
+
+print()
