d659ff1919fe5fdb5fce502805d232c8a21f89ce
armaan <armaanbadhan07@gmail.com>
2023-03-01 15:53:33 +0530

    no class

--- fun_society.py
+++ fun_society.py
@@ -1,14 +1,17 @@
+"""
+game
+"""
 import os
 import msvcrt  # check os then import msvcrt/getch
-from util.cursor import Cursor
+import sys
+
+from util.cursor import AnsiCodes
+from util.cursor import print, clear_all
 
 from games.snake import play_snake
 
 os.system('mode con: cols=100 lines=28')
 
-cur = Cursor()
-# noinspection PyShadowingBuiltins
-print = cur.tprint
 
 BANNER = [
     "███████╗██╗   ██╗███╗   ██╗     ███████╗ ██████╗  ██████╗██╗███████╗████████╗██╗   ██╗",
@@ -20,16 +23,16 @@ BANNER = [
 ]
 # TODO: change info xd
 INFO = """
-Ned Bosham reopened the arcade under the name "Fun Society Amusement, LLC". In August 2013, Harold 
-Bosham pressed his father to sell the property; when that failed, Harold stole his twin brother's 
-hunting rifle and shot Ned in the face. The rifle kicked back so hard, he tripped and fell out of 
-a 20-story window. Harold's twin brother Clyde was arrested for double homicide and sent to prison, 
-where he became Romero's cellmate. When Romero was released, he received the keys from Clyde, who 
-urged him to burn it down; instead, Romero took advantage of the building's legal limbo to 
-create an anonymous safehouse.
+Ned Bosham reopened the arcade under the name "Fun Society Amusement, LLC". In August 2013, Harold
+ Bosham pressed his father to sell the property; when that failed, Harold stole his twin brother's
+ hunting rifle and shot Ned in the face. The rifle kicked back so hard, he tripped and fell out of
+ a 20-story window. Harold's twin brother Clyde was arrested for double homicide and sent to prison,
+ where he became Romero's cellmate. When Romero was released, he received the keys from Clyde, who
+ urged him to burn it down; instead, Romero took advantage of the building's legal limbo to
+ create an anonymous safe-house.
 """
 
-# TODO: store game_name, game function, arrow params somethere
+# TODO: store game_name, game function, arrow params somewhere
 ARROW_POSITION = {  # (call param) : (down, right)
     (0, 0): (22, 16),
     (0, 1): (22, 42),
@@ -51,49 +54,52 @@ class Arrow:
     def display(self, pos):
         self.erase()
         self.position = pos
-        posi = ARROW_POSITION[pos]
-        print(self.arrow, pos_x=posi[1], pos_y=posi[0])
+        position = ARROW_POSITION[pos]
+        print(self.arrow, pos_x=position[1], pos_y=position[0])
 
     def erase(self):
         if self.position is not None:
-            posi = ARROW_POSITION[self.position]
-            print(self.eraser, pos_x=posi[1], pos_y=posi[0])
+            position = ARROW_POSITION[self.position]
+            print(self.eraser, pos_x=position[1], pos_y=position[0])
 
 
 # TODO: show score on this and "press this to continue"
 def play_games(x, y):
     if (x, y) == (0, 0):
         score = play_snake()
-        cur.clear_all()
+        clear_all()
         print(f"YOU SCORED {score}! \npress enter to continue")
         input()
-        return
     else:
         print("game will be implemented soon")
-        exit()
+        sys.exit()
 
 
 # TODO: border around the whole thing?
 def show_menu():
-    cur.clear_all()
+    """
+    Shows the menu hah
+    :return:
+    """
+    clear_all()
     print("\n\n", pos_x=0, pos_y=0)
 
     for ban in BANNER:
-        print("        " + ban + '\n', color=cur.RED)
+        print("        " + ban + '\n', color=AnsiCodes.RED)
 
-    print('\n' + INFO + '\n\n', color=cur.LIGHT_RED)
+    print('\n' + INFO + '\n\n', color=AnsiCodes.LIGHT_RED)
 
     print(" " * 30)
-    print("***Welcome to Fun-Society***\n\n", negative=True, blink=True, color=cur.GREEN)
+    print("***Welcome to Fun-Society***\n\n", negative=True, blink=True, color=AnsiCodes.GREEN)
 
-    print("choose the game you wanna play(w/a/s/d/enter):\n", color=cur.BLUE)
+    print("choose the game you wanna play(w/a/s/d/enter):\n", color=AnsiCodes.BLUE)
 
-    print(" " * 20 + "snake " + " " * 20 + "pong  " + '\n', color=cur.LIGHT_CYAN)
-    print(" " * 20 + "game 3" + " " * 20 + "game 4" + '\n', color=cur.LIGHT_CYAN)
-    print(" " * 20 + "game 5" + " " * 20 + "game 6" + '\n\n', color=cur.LIGHT_CYAN)
+    print(" " * 20 + "snake " + " " * 20 + "pong  " + '\n', color=AnsiCodes.LIGHT_CYAN)
+    print(" " * 20 + "game 3" + " " * 20 + "game 4" + '\n', color=AnsiCodes.LIGHT_CYAN)
+    print(" " * 20 + "game 5" + " " * 20 + "game 6" + '\n\n', color=AnsiCodes.LIGHT_CYAN)
 
     print(" " * 20 + "      " + " " * 20)
-    print("exit", underline=True, color=cur.LIGHT_CYAN)
+    print("exit", underline=True, color=AnsiCodes.LIGHT_CYAN)
 
     arrow = Arrow()
     point_to_x, point_to_y = 0, 0
@@ -112,7 +118,7 @@ def show_menu():
             os.system('cls')
             if point_to_x == 3:
                 print('THANKS FOR PLAYING')
-                exit()
+                sys.exit()
             play_games(point_to_x, point_to_y)
             break
 
--- util/cursor.py
+++ util/cursor.py
@@ -19,68 +19,68 @@ class AnsiCodes:
     LIGHT_CYAN = "\033[1;36m"
     LIGHT_WHITE = "\033[1;37m"
 
-    BOLD =      "\033[1m"
-    FAINT =     "\033[2m"
-    ITALIC =    "\033[3m"
+    BOLD = "\033[1m"
+    FAINT = "\033[2m"
+    ITALIC = "\033[3m"
     UNDERLINE = "\033[4m"
-    BLINK =     "\033[5m"
-    NEGATIVE =  "\033[7m"
-    CROSSED =   "\033[9m"
-    END =       "\033[0m"
+    BLINK = "\033[5m"
+    NEGATIVE = "\033[7m"
+    CROSSED = "\033[9m"
+    END = "\033[0m"
 
     RESET = "\033c"
     HOME = "\033[;H" + "\r"
 
-    UP =      '\033[1A'
-    DOWN =    '\033[1B'
+    UP = '\033[1A'
+    DOWN = '\033[1B'
     FORWARD = '\033[1C'
-    BACK =    '\033[1D'
+    BACK = '\033[1D'
 
     RESET_COLOR = '\033[0m'
 
 
-class Cursor(AnsiCodes):
-    def __init__(self) -> None:
-        pass
+def clear_all():
+    stdout.write(AnsiCodes.RESET)
 
-    def clear_all(self):
-        stdout.write(self.RESET)
 
-    def go_down_n(self, n: int):
-        stdout.write(f'\033[{n}B')
-    
-    def go_right_n(self, n: int):
-        stdout.write(f'\033[{n}C')
+def go_down_n(n: int):
+    stdout.write(f'\033[{n}B')
 
-    def tprint(self, print_this, *, pos_x:int=-1, pos_y:int=-1, color=None, reset_color:bool=True, blink:bool=False, faint:bool=False, \
-        bold=False, italic=False, underline=False, negative=False, crossed=False):
-        """
-        if pos_x or pos_y not given, continue from the cursors position
-        if any one given, go to home, then go down and/or right
-        """
-        if pos_x >= 0 or pos_y >= 0:
-            stdout.write(self.HOME)
-        if pos_y > 0:
-            self.go_down_n(pos_y)
-        if pos_x > 0:
-            self.go_right_n(pos_x)
 
-        if color is not None:
-            stdout.write(color)
-        if blink:
-            stdout.write(self.BLINK)
-        if faint:
-            stdout.write(self.FAINT)
-        if bold:
-            stdout.write(self.BOLD)
-        if italic:
-            stdout.write(self.ITALIC)
-        if underline:
-            stdout.write(self.UNDERLINE)
-        if negative:
-            stdout.write(self.NEGATIVE)
-        if crossed:
-            stdout.write(self.CROSSED)
-        stdout.write(print_this)
-        if reset_color:
-            stdout.write(self.RESET_COLOR)
+def go_right_n(n: int):
+    stdout.write(f'\033[{n}C')
+
+
+# noinspection PyShadowingBuiltins
+def print(print_this, *, pos_x: int = -1, pos_y: int = -1, color=None, reset_color: bool = True, blink: bool = False,
+          faint: bool = False, bold=False, italic=False, underline=False, negative=False, crossed=False):
+    """
+    if pos_x or pos_y not given, continue from the cursors position
+    if any one given, go home, then go down and/or right
+    """
+    if pos_x >= 0 or pos_y >= 0:
+        stdout.write(AnsiCodes.HOME)
+    if pos_y > 0:
+        go_down_n(pos_y)
+    if pos_x > 0:
+        go_right_n(pos_x)
+
+    if color is not None:
+        stdout.write(color)
+    if blink:
+        stdout.write(AnsiCodes.BLINK)
+    if faint:
+        stdout.write(AnsiCodes.FAINT)
+    if bold:
+        stdout.write(AnsiCodes.BOLD)
+    if italic:
+        stdout.write(AnsiCodes.ITALIC)
+    if underline:
+        stdout.write(AnsiCodes.UNDERLINE)
+    if negative:
+        stdout.write(AnsiCodes.NEGATIVE)
+    if crossed:
+        stdout.write(AnsiCodes.CROSSED)
+    stdout.write(print_this)
+    if reset_color:
+        stdout.write(AnsiCodes.RESET_COLOR)
